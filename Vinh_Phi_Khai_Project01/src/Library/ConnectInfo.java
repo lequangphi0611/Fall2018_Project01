04a752022885d3b0fc06b5b3aba1d49c666f5160
lequangphi0611 <lequangphi0611@gmail.com>
2018-10-30 11:30:54 +0700

    Phi

---
+++ Vinh_Phi_Khai_Project01/src/Library/ConnectInfo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Library;
+
+/**
+ *
+ * @author Quang Phi
+ */
+public class ConnectInfo {
+    public static final String USER = "";
+    public static final String PASS = "";
+    public static final String URL = "";
+    public static final String DRIVER = "";
+}
