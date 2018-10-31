e170fd8608a601f8b7bba1e404d01d72502c7ba7
lequangphi0611 <lequangphi0611@gmail.com>
2018-10-31 14:22:48 +0700

    phi

--- Vinh_Phi_Khai_Project01/src/DAO/ItemDAO.java
+++ Vinh_Phi_Khai_Project01/src/DAO/ItemDAO.java
@@ -5,10 +5,45 @@
  */
 package DAO;
 
+import Interface.IDao;
+import Model.Item;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  *
  * @author Quang Phi
  */
-public class ItemDAO {
+public class ItemDAO extends DAO<Item> implements IDao<Item,Integer>{
+
+    @Override
+    public Item getModel() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Item> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean insert(Item model) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(Item model) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(Integer object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Item> findModel(Integer object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
